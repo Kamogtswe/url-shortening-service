ea4ae3a79ba2a678f8c39927fdcfaf80bc458b51
Kamogetswe Machaka <machakathato@gmail.com>
2023-08-14 21:25:20 +0200

    Hello world test

--- src/main/java/com/kamogtswe/urlShortService/UrlShortServiceApplication.java
+++ src/main/java/com/kamogtswe/urlShortService/UrlShortServiceApplication.java
@@ -2,12 +2,23 @@ package com.kamogtswe.urlShortService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class UrlShortServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UrlShortServiceApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String helloWorld(@RequestParam
+	(value = "name", defaultValue = "World") String name)
+	{
+		return String.format("Hello %s!", name);
 	}
 
 }
