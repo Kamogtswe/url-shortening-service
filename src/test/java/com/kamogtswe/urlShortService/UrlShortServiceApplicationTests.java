bd9ac9bf99e4fbb7228a64ef112c96b41fc7bcd3
Kamogetswe Machaka <root@LAPTOP-RTFSGLOM.localdomain>
2023-08-14 20:37:36 +0200

    initial commit

---
+++ src/test/java/com/kamogtswe/urlShortService/UrlShortServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kamogtswe.urlShortService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
